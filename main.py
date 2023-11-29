f526c171ae4b654c56e3e384192b7594a5315ef1
Jeffrey Scott <valenpendragon@gmail.com>
2023-11-29 15:02:17 -0500

    Add main.py. Add import statements and DATA_DIRECTORY configuration.

---
+++ main.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QFileDialog,
+                               QMessageBox, QApplication, QMainWindow, QStatusBar,
+                               QLabel, QHBoxLayout)
+
+import sys
+import os
+
+DATA_DIRECTORY = 'data/'
