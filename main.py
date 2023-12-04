f8081fa63e69e4c2964df1cf0894c06bc4012a2e
Jeffrey Scott <valenpendragon@gmail.com>
2023-12-04 13:21:17 -0500

    Add class StartupWindow with __init__() method and skeletons of methods load_tables(), load_config_file(), and init_ui().

--- main.py
+++ main.py
@@ -11,3 +11,41 @@ DAMAGE_TYPES = f'{DATA_DIRECTORY}/damage_types.json'
 HIGHLIGHTING = f'{DATA_DIRECTORY}/highlighting.json'
 TABLES = f'{DATA_DIRECTORY}/tables.xlsx'
 CONFIG_FILE = f'{DATA_DIRECTORY}/config.json'
+
+
+class StartWindow(QMainWindow):
+    def __init__(self, conditions_fp=CONDITIONS, damage_types_fp=DAMAGE_TYPES,
+                 highlighting_fp=HIGHLIGHTING, table_fp=TABLES, config_file=CONFIG_FILE):
+        super().__init__()
+        # Initialize to None any attributes handled by other methods,
+        # such as init_ui, load_tables, load_config_files.
+        self.conditions_fp = conditions_fp
+        self.damage_types_fp = damage_types_fp
+        self.highlighting_fp = highlighting_fp
+        self.tables_fp = table_fp
+        self.config_fp = config_file
+        self.vbox = None
+        self.statusbar = None
+        self.required_tables = None
+        self.treasure_window = None
+        self.load_config_files()
+        self.load_tables()
+        self.init_ui()
+
+    def load_tables(self):
+        pass
+
+    def load_config_files(self):
+        pass
+
+    def init_ui(self):
+        pass
+
+
+if __name__ == "__main__":
+    sys.argv += ['-platform', 'windows:darkmode=2']
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    window = StartWindow()
+    window.show()
+    sys.exit(app.exec())
