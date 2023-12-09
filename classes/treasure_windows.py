cf7bf12dccfbbd38cff1bda25c42f01ae8e39bf7
Jeffrey Scott <valenpendragon@gmail.com>
2023-12-09 05:01:06 -0500

    Remove unnecessary CONSTANTS from treasure_windows.py.

--- classes/treasure_windows.py
+++ classes/treasure_windows.py
@@ -7,13 +7,6 @@ import os
 import pandas as pd
 import openpyxl
 
-DATA_DIRECTORY = 'data/'
-CONDITIONS = f'{DATA_DIRECTORY}/conditions.json'
-DAMAGE_TYPES = f'{DATA_DIRECTORY}/damage_types.json'
-HIGHLIGHTING = f'{DATA_DIRECTORY}/highlighting.json'
-TABLES = f'{DATA_DIRECTORY}/tables.xlsx'
-CONFIG_FILE = f'{DATA_DIRECTORY}/config.json'
-
 
 class TreasureWindow(QMainWindow):
     pass
