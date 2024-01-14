8748b832f78adbcbfc81606502dbed64c9dec5c5
Jeffrey Scott <valenpendragon@gmail.com>
2024-01-14 00:15:05 -0500

    Add functionality check_worksheet function to data_checks.py to make sure that the tables do not have gaps or overlaps in the roll numbering. Create test_workbook.xlsx with options to validate data_checks functions: check_worksheet, find_workbooks, and check_workbook. Remove check for bool type in roll_test since True and False are also integer values.

--- functions/data_checks.py
+++ functions/data_checks.py
@@ -1,6 +1,6 @@
 import os.path
-
 import pandas as pd
+import numpy as np
 
 
 def roll_test(s):
@@ -23,8 +23,6 @@ def roll_test(s):
         return (s,)
     elif isinstance(s, float):
         return (int(s),)
-    elif isinstance(s, bool):
-        raise ValueError(f"roll_test: argument cannot be bool type.")
     elif isinstance(s, str):
         l = s.split('-')
         try:
@@ -161,6 +159,83 @@ def find_workbooks(wb_fp_list):
     return data
 
 
+def check_worksheet(table: pd.read_table) -> bool:
+    """
+    This function analyzes the table supplied to it. There may be index
+    columns which will be ignored. The focus is on roll columns and
+    description columns. The former should have a header indicating the
+    type of dice to be used. The latter is text describing a result. Either
+    column may have entries listed as None. For the roll column, that
+    indicates the result in the description is not possible. For the
+    description column, that means that the roll value or range produces
+    nothing. Note: A Dash in a column will be treated as a blank entry.
+
+    The roll columns are the part that requires carefully validation.
+    There can be blank values in a roll column, but numbers must be sequential
+    otherwise. There can be no duplication of values from row to row, no
+    overlaps, and no gaps. Otherwise, the table is not usable.
+
+    This function returns a bool, True if the worksheet is usable, False
+    otherwise.
+    :param table: pd.DataFrame
+    :return: bool
+    """
+    headers = table.columns
+    # Find the roll column first. All unnamed columns will be ignored.
+    # The order of the possible values ensures the largest match will
+    # be the first one found.
+    possible_roll_values = ('d1000', 'd100', 'd30', 'd20',
+                            'd12', 'd10', 'd8', 'd6',
+                            'd4', 'd2')
+    roll_header = None
+    roll_type = None
+    for h in headers:
+        for v in possible_roll_values:
+            if v in h.lower().strip():
+                roll_type = v
+                roll_header = h
+                break
+    if roll_header is None:
+        # There is no roll column that matches the criteria.
+        print("check_worksheet: No roll column founds Table is an invalid format.")
+        return False
+    else:
+        roll_column = table[roll_header]
+    roll_max = int(roll_type[1:])
+    print(f"check_worksheet: roll_header: {roll_header}. roll_type: {roll_type}. "
+          f"roll_max: {roll_max}. roll_column: {roll_column}.")
+    last_val = 0
+    for item in roll_column:
+        print(f"check_worksheet: last_val: {last_val}. item: {item}.")
+        if item is None or item == '-':
+            # Skip blank entries.
+            continue
+        else:
+            item_ck = roll_test(str(item))
+            print(f"check_worksheet: item_ck: {item_ck}.")
+            if item_ck[0] is False:
+                # If roll_test returned (False,), there is a bad entry in the
+                # roll column. 0 is also an invalid entry, since rolls start
+                # with 1.
+                return False
+            else:
+                if last_val + 1 != item_ck[0]:
+                    # There is a gap or an overlap.
+                    return False
+                else:
+                    if len(item_ck) == 2:
+                        last_val = item_ck[1]
+                    else:
+                        last_val = item_ck[0]
+    print(f"check_worksheet: last_val: {last_val}. roll_max: {roll_max}.")
+    if last_val != roll_max:
+        # Either the values goes over the maximum dice value or under it. Either
+        # way, the roll column is invalid.
+        return  False
+    else:
+        return True
+
+
 # TODO: Create check_worksheet to verify that the numbering in the table
 #  is consistent with the column header of the table and has no gaps or
 #  overlaps in the numbering.
@@ -171,3 +246,25 @@ if __name__ == "__main__":
         print(f"Test results for {item} is {roll_test(item)}")
     # TODO: Add tests for check_workbook and find_workbooks using
     #  sample files included in data directory.
+    wb_fp = '../data/test_workbook.xlsx'
+    ws_list = ['Treasure For CR 0 Magic', 'Treasure For CRs 1-2 Coin',
+               'Treasure For CRs 1-2 Magic', 'Treasure For CRs 3-4 Coin',
+               'Treasure For CRs 3-4 Magic', 'Treasure for CR 0 Coin'
+               'Treasure For CR 0 Coin']
+    f = pd.ExcelFile(wb_fp)
+    tables = {}
+    for ws in ws_list:
+        try:
+            df = pd.read_excel(wb_fp, sheet_name=ws, index_col=0, na_values=True)
+        except ValueError:
+            print(f"Worksheet, {ws}, is not in workbook, {wb_fp}.")
+        else:
+            tables[ws] = df.replace(to_replace=np.nan, value=None)
+    f.close()
+    print(f"Collected tables: {tables}.")
+    for ws in tables.keys():
+        print(f"Testing table, {ws}.")
+        if check_worksheet(tables[ws]):
+            print(f"Table, {ws}, has a valid format and is usable.")
+        else:
+            print(f"Table, {ws}, has an invalid format and cannot be used.")
