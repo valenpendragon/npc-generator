3beeba81acd85e40957d91b2ec842743d05478fb
Jeffrey Scott <valenpendragon@gmail.com>
2024-01-17 07:46:24 -0500

    Change TreasureWindow to use inherit from QMainWindow. Add statusBar and methodology to TreasureWindow attributes. Create TreasureWindow.init_ui method and move all UI changes to it. Add close button to TreasureWindow.

--- classes/treasure_windows.py
+++ classes/treasure_windows.py
@@ -8,26 +8,50 @@ import pandas as pd
 import openpyxl
 
 
-class TreasureWindow(QWidget):
+class TreasureWindow(QMainWindow):
     def __init__(self, config: dict, conditions: dict,
                  damage_types: dict, highlighting: dict,
                  tables: dict, parent=None):
         print(f"treasure_windows: Starting TreasureWindow.__init__().")
         super().__init__(parent)
-        self.setMinimumSize(800, 600)
-        self.setWindowTitle("Treasure Generation Window")
-        main_layout = QGridLayout()
 
         # Set attributes pushed from calling window.
         self.config = config
         self.conditions = conditions
         self.damage_types = damage_types
         self.highlighting = highlighting
+        self.tables = tables
 
         # Set minimum values for new attributes.
         self.encounter_challenge_rating = 0
         self.elite_creature = False
         self.legendary_creature = False
+        self.methodology = 'CR'
+
+        # Setup statusbar.
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        # Initialize the grig.
+        self.grid = None
+
+        # Run init_ui.
+        self.init_ui()
         print(f"treasure_windows: Completed TreasureWindow.__init__().")
+
+    def init_ui(self):
+        self.setMinimumSize(800, 600)
+        self.setWindowTitle("Treasure Generation Window")
+
+        close_button = QPushButton("Close")
+        close_button.clicked.connect(self.close)
+
+        self.setCentralWidget(QWidget(self))
+        self.grid = QGridLayout()
+        self.centralWidget().setLayout(self.grid)
+        self.grid.addWidget(close_button)
+
+        print(f"TW.init_ui: Completed TreasureWindows.init_ui().")
+
 
 
