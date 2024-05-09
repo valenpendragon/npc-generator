9737871fb2752aa4e143ce2e7392a6faf2922f96
Jeffrey Scott <valenpendragon@gmail.com>
2024-05-09 16:04:57 -0400

    Complete MagicItem class.

--- entities/treasures.py
+++ entities/treasures.py
@@ -1,5 +1,14 @@
 class MagicItem:
-    pass
+    """
+    This class handles magic items in RPGs. Since the tables only contain
+    the name of the item, that is the only attribute available.
+    """
+    def __init__(self, name: str):
+        self.item_name = name
+
+    def __str__(self):
+        s = f"Magic Item: {self.item_name}"
+        return s
 
 
 class OtherWealth:
